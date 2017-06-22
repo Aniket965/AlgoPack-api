4f33955eb6d299f487002dd9546150a431dd54a3
aniket965 <aniket965.as@gmail.com>
2017-06-22 19:24:43 +0530

    added  something more 😂😂

--- apiBuilder.js
+++ apiBuilder.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var colors = require('colors');
 var exec = require('child_process').exec;
 var rimraf = require('rimraf');
+var jsonfile = require('jsonfile');
+
 // module.exports = function () {
 var apiDir = process.cwd() + '/_api';
 
@@ -17,13 +19,35 @@ var adsnRepoDir = process.cwd() + '/Algo_Ds_Notes';
 // }
 
 // clone_adns_repo();
- 
+
 // deleting git repo from cloned repository
-rimraf.sync(adsnRepoDir+'/.git');
+rimraf.sync(adsnRepoDir + '/.git');
 
 var algoNames = dirs(process.cwd() + '/Algo_Ds_Notes')
-console.log(algoNames);
+// console.log(algoNames);
 
+var data = {};
+
+algoNames.forEach(function (algo) {
+
+    var algoDir = adsnRepoDir + '/' + algo;
+
+    // Reades files dir
+    var algoList = [];
+    fs.readdirSync(algoDir).forEach(file => {
+        algoList.push(file);
+    });
+
+    // console.log(algoList);
+
+    data[algo] = algoList;
+
+});
+
+var mainAPI_Dir  = process.cwd() + '/_api/algoList.json';
+jsonfile.writeFile(mainAPI_Dir, data,{spaces: 2}, function (err) {
+  console.error(err)
+})
 
 
 
@@ -32,9 +56,9 @@ console.log(algoNames);
 function clone_adns_repo() {
     exec('git clone https://github.com/algobook/Algo_Ds_Notes.git', (err, stdout, stderr) => {
         if (err) {
-            console.error(err);console.log("cloned");
-        }else {
-                console.log("cloned🎉🎉🎉🎉🎉".green);
+            console.error(err); console.log("cloned");
+        } else {
+            console.log("cloned🎉🎉🎉🎉🎉".green);
         }
     });
 }
@@ -42,6 +66,6 @@ function clone_adns_repo() {
  * returns dirs in path
  */
 function dirs(p) {
-    return fs.readdirSync(p).filter(f => fs.statSync(p+"/"+f).isDirectory())
+    return fs.readdirSync(p).filter(f => fs.statSync(p + "/" + f).isDirectory())
 }
 // }
